e9309cf082478e3e04a606b5069ec0280072bb86
Tejas Pagooah <tejas@lsl.digital>
2018-02-06 23:56:47 +0400

    Initial commit with Nuxt.js and coming soon intro. (Fonts included & Favicons)

---
+++ nuxt.config.js
@@ -0,0 +1,90 @@
+/*
+  @nirvan TBR'ed the meta tags
+*/
+
+module.exports = {
+  head: {
+    title: 'Developers Conference 2018',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'robots', content: 'index,follow' },
+      { name: 'googlebot', content: 'index,follow' },
+      { name: 'rights', content: '© Mauritius Software Craftsmanship Community (MSCC) - 2018' },
+      { name: 'origin', content: '/images/og-image.jpg' },
+      { name: 'author', content: 'Mauritius Software Craftsmanship Community (MSCC)' },
+      { hid: 'description', name: 'description', content: 'Developers Conference 2018 Official Website' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:title', property: 'og:title', content: 'Developers Conference 2018' },
+      { hid: 'og:url', property: 'og:url', content: 'https:/www.conference.mscc.mu' },
+      { hid: 'og:image', property: 'og:image', content: '/images/og-image.jpg' },
+      { hid: 'og:description', property: 'og:description', content: 'Developers Conference 2018 Official Website' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'Developers Conference 2018' },
+      { hid: 'og:locale', property: 'og:locale', content: 'en_US' },
+      { hid: 'twitter:site', name: 'twitter:site', content: '@MSCraftsman' },
+      { hid: 'twitter:card', name: 'twitter:site', content: 'summary_large_image' },
+      { hid: 'twitter:title', name: 'twitter:title', content: 'Developers Conference 2018' },
+      { hid: 'twitter:url', name: 'twitter:url', content: 'https:/www.conference.mscc.mu' },
+      { hid: 'twitter:image', name: 'twitter:image', content: '/images/og-image.jpg' },
+      { hid: 'twitter:description', name: 'twitter:description', content: 'Developers Conference 2018 Official Website' },
+      { hid: 'twitter:creator', name: 'twitter:creator', content: '@MSCraftsman' },
+      { hid: 'fb:admins', property: 'fb:admins', content: '' },
+      { hid: 'fb:app_id', property: 'fb:app_id', content: '' },
+      { hid: 'fb:pages', property: 'fb:pages', content: '181737551985316' },
+      { name: 'msapplication-square70x70logo', content: '/images/favicon/smalltile.png' },
+      { name: 'msapplication-square150x150logo', content: '/images/favicon/mediumtile.png' },
+      { name: 'msapplication-wide310x150logo', content: '/images/favicon/widetile.png' },
+      { name: 'msapplication-square310x310logo', content: '/images/favicon/largetile.png' },
+      { name: 'theme-color', content: '#004a8f' },
+      { name: 'msapplication-TileColor', content: '#004a8f' },
+      { hid: 'article:author', property: 'article:author', content: 'Mauritius Software Craftsmanship Community (MSCC)' },
+      { hid: 'name', itemprop: 'name', content: 'Developers Conference 2018' },
+      { hid: 'description', itemprop: 'description', content: 'Developers Conference 2018 Official Website' },
+      { hid: 'image', itemprop: 'image', content: '/images/og-image.jpg' }  
+    ],
+    link: [
+      { rel: 'canonical', href: 'https:/www.conference.mscc.mu' },
+      { rel: 'icon', type: 'image/x-icon', href: '/images/favicon/favicon.ico' },
+      { rel: 'stylesheet', href: 'https:/fonts.googleapis.com/css?family=Roboto:100,300,400,500,700'},
+      { rel: 'stylesheet', href: 'https:/use.fontawesome.com/releases/v5.0.6/css/all.css'},
+      { rel: 'shortcut icon', type: 'image/x-icon', href: '/images/favicon/favicon.ico' },
+      { rel: 'apple-touch-icon', sizes: '57x57', href: '/images/favicon/apple-touch-icon-57x57.png' },
+      { rel: 'apple-touch-icon', sizes: '60x60', href: '/images/favicon/apple-touch-icon-60x60.png' },
+      { rel: 'apple-touch-icon', sizes: '72x72', href: '/images/favicon/apple-touch-icon-72x72.png' },
+      { rel: 'apple-touch-icon', sizes: '76x76', href: '/images/favicon/apple-touch-icon-76x76.png' },
+      { rel: 'apple-touch-icon', sizes: '114x114', href: '/images/favicon/apple-touch-icon-114x114.png' },
+      { rel: 'apple-touch-icon', sizes: '120x120', href: '/images/favicon/apple-touch-icon-120x120.png' },
+      { rel: 'apple-touch-icon', sizes: '144x144', href: '/images/favicon/apple-touch-icon-144x144.png' },
+      { rel: 'apple-touch-icon', sizes: '152x152', href: '/images/favicon/apple-touch-icon-152x152.png' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/images/favicon/apple-touch-icon-180x180.png' },
+      { rel: 'icon', sizes: '16x16', type: 'image/png', href: '/images/favicon/favicon-16x16.png' },
+      { rel: 'icon', sizes: '32x32', type: 'image/png', href: '/images/favicon/favicon-32x32.png' },
+      { rel: 'icon', sizes: '96x96', type: 'image/png', href: '/images/favicon/favicon-96x96.png' },
+      { rel: 'icon', sizes: '192x192', type: 'image/png', href: '/images/favicon/android-chrome-192x192.png' },
+      { rel: 'publisher', href: 'https:/plus.google.com/+lexpressmu' }
+    ]
+  },
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  css: [
+    '@/assets/style/main.scss'
+  ],
+  mode: 'spa',
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,159 @@
+<template>
+  <div>
+    <section class="main full-section-container">
+      <div class="logo-container">
+        <div class="logo-wrapper">
+          <img src="~/assets/images/logo.svg" alt="MSCC Logo" class="logo-svg">
+          <h1 class="title">Developers Conference</h1>
+          <div class="year-wrapper">
+            2018
+          </div>
+        </div>
+
+        <div class="footer-text-wrapper">
+          <h3>Coming soon!</h3>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+/*
+  * tejas@pagooah.com 
+*/
+export default {
+  components: {
+
+  },
+  data () {
+    return {
+      
+    }
+  },
+  methods: {
+
+  },
+  computed: {
+
+  },
+  mounted () {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+:root {
+  --color-green: #31e8b7;
+  --color-blue: #2847d9;
+  --color-white: #fff;
+  --color-black: #333;
+  --font-glacial: "GlacialIndifference";
+  --font-shentox: "Shentox";
+  --gutter: 20px;
+}
+
+$tablet: 1024px;
+$tablet-portrait: 768px;
+$mobiie: 540px;
+
+.full-section-container {
+  display: grid;
+  grid-template-columns: [full-start] 1fr [container-start] minmax(300px, 1300px) [container-end] 1fr [full-end];
+}
+
+* {
+  box-sizing: border-box;
+}
+
+.main {
+  background: linear-gradient(135deg, rgba(49,232,183, 0.8) 0%, rgba(40,71,217, 0.8) 100%), url(~/static/images/home-bg-2.jpg);
+  background-size: cover, cover;
+  background-position: center center;
+  height: 100vh;
+
+  .logo-container {
+    grid-column: container;    
+    display: grid;
+    height: 100vh;
+
+    .logo-wrapper {
+      text-align: center;
+      align-self: end;
+
+      .logo-svg {
+        height: 200px;
+      }
+
+      .title {
+        font-family: var(--font-glacial);
+        text-transform: uppercase;
+        color: var(--color-white);
+        font-size: 80px;
+        font-weight: 500;
+        margin: calc(var(--gutter) / 1);
+        text-shadow: 0 10px 50px rgba(0, 0, 0, 0.05);
+      }
+
+      .year-wrapper {
+        display: inline-block;
+        font-family: var(--font-shentox);
+        text-transform: uppercase;
+        color: var(--color-white);
+        font-size: 80px;
+        line-height: 50px;
+        padding: var(--gutter);
+        
+        padding-top: calc(var(--gutter) + 10px);
+        font-weight: 700;
+        background: linear-gradient(135deg, rgba(49,232,183, 1) 0%, rgba(40,71,217, 1) 100%);
+        border-radius: 5px;
+        box-shadow: 0 10px 50px rgba(0, 0, 0, 0.2);
+      }
+    }
+
+    .footer-text-wrapper {
+      align-self: end;
+      text-align: center;
+      padding-bottom: calc(var(--gutter) + 10px);
+
+      h3 {
+        color: var(--color-white);
+        font-family: var(--font-shentox);
+        font-weight: 500;
+        text-transform: uppercase;
+        font-size: 40px;
+      }
+    }
+  }
+}
+
+
+@media (max-width: $tablet-portrait) {
+  .main {
+    .logo-container {
+      .logo-wrapper {
+        .logo-svg {
+          max-height: 140px;
+        }
+        .title {
+          font-size: 50px;
+        }
+        .year-wrapper {
+          font-size: 50px;
+          line-height: 30px;
+          padding-top: calc(var(--gutter) + 5px);
+        }
+      }
+      .footer-text-wrapper {
+        h3 {
+          font-size: 25px;
+        }
+      }
+    }
+  }
+}
+
+</style>
