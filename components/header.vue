706ce451fbb43690c4b6b86c3d012ce67c114b91
Tejas Pagooah <tejas@lsl.digital>
2018-02-11 02:00:31 +0400

    Re-designed main layout + Header + Added counter

---
+++ components/header.vue
@@ -0,0 +1,181 @@
+<template>
+  <div class="full-section-container fixed">
+    <header class="main-header">
+      <div class="logo-wrapper">
+        <nuxt-link to="/">
+          <img src="~/assets/images/logo.svg" alt="MSCC Logo" class="logo-svg">
+        </nuxt-link>
+      </div>
+      <div class="menu-wrapper">
+        <ul class="menu-items">
+          <li class="menu-item" :key="index" v-for="(item, index) in menuItems"><nuxt-link :to="item.url">{{item.name}}</nuxt-link></li>
+        </ul>
+      </div>
+      <div class="register-button">
+        <nuxt-link to="/">
+          <span>Register</span>
+        </nuxt-link>
+      </div>
+    </header>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      menuItems: [
+        {
+          name: 'Home',
+          url: '/'
+        },
+        {
+          name: 'Sessions',
+          url: '/sessions'
+        },
+        {
+          name: 'Speakers',
+          url: '/speakers'
+        },
+        {
+          name: 'Extras',
+          url: '/extras'
+        },
+        {
+          name: 'Hackathon',
+          url: '/hackathon'
+        },
+        {
+          name: 'Sponsors',
+          url: '/sponsors'
+        },
+        {
+          name: 'Contact',
+          url: '/contact'
+        }
+      ]
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+
+  $tablet: 1024px;
+  $tablet-portrait: 768px;
+  $mobiie: 540px;
+
+  .full-section-container {
+    display: grid;
+    width: 100%;
+    grid-template-columns: [full-start] 1fr [container-start] minmax(300px, 1300px) [container-end] 1fr [full-end];
+
+    &.fixed {
+      position: fixed;
+      top: 0px;   
+    }
+  }
+
+  .main-header {
+    width: 100%;
+    display: grid;
+    height: 80px;
+    grid-column: full;
+    grid-template-columns: 1fr 5fr 1fr;
+    grid-template-rows: 1;
+    align-items: center;
+    box-shadow: 0 3px 5px rgba(0, 0, 0, 0.1);
+
+    .logo-wrapper {
+      grid-column: 1 / 2;
+      height: 80px;
+      background: linear-gradient(135deg, rgba(49,232,183, 1), rgba(40,71,217, 1));
+      display: grid;
+      align-items: center;
+      justify-content: center;
+      transform: skewX(-15deg) scaleX(1.1);
+      
+
+      a {
+        display: block;
+        height: 60px;
+        transform: skewX(15deg) scaleX(0.95);
+
+        img {
+          height: 60px;
+        }
+      }
+    }
+
+    .menu-wrapper {
+      grid-column: 2 / 3;
+      text-align: right;
+      height: 80px;
+      background: #fff;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      
+
+      .menu-items {
+        &, .menu-item {
+          display: inline-block;
+          margin: 0;
+          padding: 0;
+          list-style-type: none;
+          align-self: center;
+        }
+        
+        .menu-item {
+          font-size: 23px;
+
+          &:after {
+            content: '/';
+            padding: 0 calc(var(--gutter) / 1);
+            font-family: var(--font-shentox);            
+            color: #ccc;
+          }
+
+          a {
+            color: var(--color-black);
+            font-family: var(--font-shentox);
+            text-transform: uppercase;
+            font-weight: 400;
+            text-decoration: none;
+            //text-shadow: 0 2px 3px rgba(0, 0, 0, 0.4);
+          }
+
+          &:last-child {
+            margin-right: 0;
+
+            &:after {
+              content: none;
+            }
+          }
+        }
+      }
+    }
+
+    .register-button {
+      grid-column: 3 / 4;
+      background: var(--color-blue);
+      transform: skewX(-15deg) scaleX(1.1);
+      
+      a {
+        color: var(--color-white);
+        font-family: var(--font-shentox);
+        text-transform: uppercase;
+        font-weight: 400;
+        text-decoration: none;
+        font-size: 23px;
+        display: block;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 80px;
+        transform: skewX(15deg) scaleX(0.95);
+        
+      }
+    }
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,31 @@
 <template>
   <div>
+    <header-section></header-section>
     <section class="main full-section-container">
       <div class="logo-container">
         <div class="logo-wrapper">
-          <img src="~/assets/images/logo.svg" alt="MSCC Logo" class="logo-svg">
+          <!-- <img src="~/assets/images/logo.svg" alt="MSCC Logo" class="logo-svg"> -->
           <h1 class="title">Developers Conference</h1>
           <div class="year-wrapper">
             2018
           </div>
         </div>
+        <div class="description-wrapper">
+          The largest tech conference in Mauritius!
+        </div>
+        <div class="timer-wrapper">
+          <div class="timer">
+            <no-ssr>
+              <Countdown deadline="May 17, 2018"></Countdown>
+            </no-ssr>
+          </div>
+          <div class="register">
+            <nuxt-link to="/">Register</nuxt-link> 
+          </div>
+        </div>
 
         <div class="footer-text-wrapper">
-          <h3>Coming soon!</h3>
+          <h3>17 - 18 May — Voila Hotel & Flying Dodo, Bagatelle</h3>
         </div>
       </div>
     </section>
@@ -22,9 +36,16 @@
 /*
   * tejas@pagooah.com 
 */
+
+import headerSection from '~/components/header.vue'
+import Countdown from '~/components/Countdown.vue'
+import NoSSR from 'vue-no-ssr'
+
 export default {
   components: {
-
+    headerSection,
+    Countdown,
+    'no-ssr': NoSSR
   },
   data () {
     return {
@@ -68,49 +89,121 @@ $mobiie: 540px;
   box-sizing: border-box;
 }
 
+@keyframes home {
+	0% {
+		background-position: 0% 50%, center center;
+	}
+	50% {
+		background-position: 100% 50%, center center;
+	}
+	100% {
+		background-position: 0% 50%, center center;
+	}
+}
+
 .main {
-  background: linear-gradient(135deg, rgba(49,232,183, 0.8) 0%, rgba(40,71,217, 0.8) 100%), url(~/static/images/home-bg-2.jpg);
-  background-size: cover, cover;
+  background: linear-gradient(135deg, rgba(49,232,183, 0.9) 0%, rgba(40,71,217, 0.4) 50%), url(~/static/images/home-bg-2.jpg);
+  background-size: 100%, cover;
   background-position: center center;
   height: 100vh;
+
+  // animation-name: home;
+  // animation-timing-function: ease-in-out;
+  // animation-iteration-count: infinite;
+  // animation-duration: 5s;
 
   .logo-container {
     grid-column: container;    
     display: grid;
-    height: 100vh;
+    grid-auto-rows: auto;
+    height: calc( 100vh - 80px);
+    margin-top: 80px;
 
     .logo-wrapper {
-      text-align: center;
+      text-align: left;
       align-self: end;
+      display: flex;
+      align-items: center;
+      margin-bottom: calc(var(--gutter) * 2);
 
       .logo-svg {
-        height: 200px;
+        height: 40px;
+        margin-right: calc(var(--gutter) / 1);
       }
 
       .title {
-        font-family: var(--font-glacial);
-        text-transform: uppercase;
-        color: var(--color-white);
-        font-size: 80px;
-        font-weight: 500;
-        margin: calc(var(--gutter) / 1);
-        text-shadow: 0 10px 50px rgba(0, 0, 0, 0.05);
-      }
-
-      .year-wrapper {
-        display: inline-block;
         font-family: var(--font-shentox);
         text-transform: uppercase;
         color: var(--color-white);
-        font-size: 80px;
-        line-height: 50px;
-        padding: var(--gutter);
-        
-        padding-top: calc(var(--gutter) + 10px);
+        font-size: 40px;
+        line-height: 60px;
+        font-weight: 500;
+        margin: 0;
+        text-shadow: 0 10px 50px rgba(0, 0, 0, 0.05);
+        margin-right: calc(var(--gutter) / 1);
+      }
+
+      .year-wrapper {
+        display: inline-flex;
+        font-family: var(--font-shentox);
+        text-transform: uppercase;
+        color: var(--color-white);
+        font-size: 50px;
+        line-height: 30px;
+        padding: 21px 15px 16px 15px;
+        height: 68px;
         font-weight: 700;
         background: linear-gradient(135deg, rgba(49,232,183, 1) 0%, rgba(40,71,217, 1) 100%);
         border-radius: 5px;
         box-shadow: 0 10px 50px rgba(0, 0, 0, 0.2);
+      }
+    }
+
+    .description-wrapper {
+      color: var(--color-white);
+      font-size: 100px;
+      font-family: var(--font-glacial);
+      text-transform: uppercase;
+    }
+
+    .timer-wrapper {
+      display: grid;
+      width: 100%;
+      grid-template-columns: 1fr 1fr;
+      grid-gap: calc(var(--gutter) * 2);
+      align-items: center;
+
+      .timer {
+        grid-column: 1 / 2;
+        justify-self: end;
+      }
+
+      .register {
+        grid-column: 2 / 3;
+        justify-self: start;
+        width: 100%;
+
+        
+        a {
+          height: 70px;
+          background: var(--color-blue);
+          border-radius: 3px;
+          font-family: var(--font-shentox);
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          font-weight: 500;
+          color: var(--color-white);
+          text-transform: uppercase;
+          font-size: 30px;
+          text-decoration: none;
+          width: 60%;
+        }
+      }
+
+      .vue-countdown {
+        font-family: var(--font-glacial);
+        color: var(--color-green);
       }
     }
 
@@ -124,7 +217,7 @@ $mobiie: 540px;
         font-family: var(--font-shentox);
         font-weight: 500;
         text-transform: uppercase;
-        font-size: 40px;
+        font-size: 30px;
       }
     }
   }
