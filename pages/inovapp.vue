aaaa3a90fcae93af16bd70ec17ce7d47e264d060
Jochen Kirstaetter <joki@ios.mu>
2018-04-09 21:34:44 +0400

    fix structure

--- pages/inovapp.vue
+++ pages/inovapp.vue
@@ -5,21 +5,19 @@
       <h2>MCB InovApp Challenge</h2>
     </div>
     <div class="section-description-wrapper">
-      <div class="inovapp-container" v-if="hasSpeakers">
-          <div>
+      <div class="inovapp-container">
+        <div>
             MSCC is proud to support the MCB <a href="https://www.mcb.mu/inovapp/" target="_blank" rel="noopener">InovApp Challenge</a>.
-          </div>
-          <div>
+        </div>
+        <div>
             The MCB InovApp Challenge aims to identify brilliant people and teams - including start-ups and SMEs - who can ally creativity, innovation and technology to design next-gen solutions for the Mauritian banking sector. 
-          </div>
         </div>
       </div>
-      <div class="button-container">
+    </div>
+    <div class="button-container">
         <a href="https://www.mcb.mu/inovapp/" target="_blank" rel="noopener" class="button">InovApp Challenge</a>
-      </div>
     </div>
   </div>
-
 </template>
 
 <script></script>
