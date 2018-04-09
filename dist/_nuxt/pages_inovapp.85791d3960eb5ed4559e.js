cef83d4b8a1ea4fb3ba90609b66ab66b17d49cec
Jochen Kirstaetter <joki@ios.mu>
2018-04-09 21:35:13 +0400

    generate latest static

--- dist/_nuxt/pages_inovapp.85791d3960eb5ed4559e.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([5],{"6Guo":function(e,l,u){"use strict";Object.defineProperty(l,"__esModule",{value:!0});var n=u("VU/8")(null,null,!1,null,null,null);n.options.__file="pages\\inovapp.vue",l.default=n.exports}});
