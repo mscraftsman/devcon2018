301d49c0df025d3023e1014a7be204cdbeac192c
Jochen Kirstaetter <joki@ios.mu>
2018-05-05 18:48:14 +0400

    add Extras details for each user group

--- components/extra-main.vue
+++ components/extra-main.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="full-section-container" id="extras">
-    <div class="section-title-wrapper">
-      <small>It's not all about sessions</small>
-      <h2>Extra Activities</h2>
-    </div>
-    <div class="section-description-wrapper">
+    <div class="container">
+      <div class="page-title-wrapper">
+        <small>It's not all about sessions</small>
+        <h1>Extra Activities</h1>
+        <p></p>
+      </div>
     </div>
   </div>
 </template>
@@ -12,78 +13,5 @@
 
 </script>
 <style lang="scss" scoped>
-  $tablet: 1024px;
-  $tablet-portrait: 768px;
-  $mobile: 540px;
 
-  .full-section-container {
-    display: grid;
-    grid-template-columns: [full-start] 1fr [container-start] minmax(300px, 1300px) [container-end] 1fr [full-end];
-    background: transparent;
-    background-size: cover, cover;
-    padding: calc(var(--gutter) * 3) calc(var(--gutter) / 2);
-  }
-
-  * {
-    box-sizing: border-box;
-  }
-
-  .section-title-wrapper {
-    margin-bottom: calc(var(--gutter) * 3);
-    grid-column: container;
-    text-align: center;
-
-    small {
-      margin: 0 0 10px 0;
-      text-transform: uppercase;
-      color: var(--color-blue);
-      font-family: var(--font-shentox);
-      font-size: 18px;
-    }
-    h2 {
-      margin: 0;
-      text-transform: uppercase;
-      font-size: 53px;
-      line-height: 55px;
-      font-family: var(--font-glacial);
-
-    }
-
-    p {
-      line-height: 25px;
-      font-size: 18px;
-    }
-  }
-
-  .section-description-wrapper {
-  }
-
-  @media (max-width: $tablet) {
-    .section-title-wrapper {
-      h2 {
-        font-size: 45px;
-        line-height: 50px;
-      }
-    }
-
-    .section-description-wrapper {
-    }
-  }
-
-  @media (max-width: $tablet-portrait) {
-    .section-description-wrapper {
-    }
-  }
-
-  @media (max-width: $mobile) {
-    .section-title-wrapper {
-      h2 {
-        font-size: 35px;
-        line-height: 40px;
-      }
-    }
-
-    .section-description-wrapper {
-    }
-  }
 </style>
---
+++ components/extra-maker.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="full-section-container">
+    <div class="container">
+      <div class="section-title-wrapper">
+        <small>DIY projects - Raspberry Pi, Arduino, any...</small>
+                    <h2>Mauritius Maker Community</h2>
+        </div>
+      <div class="content-wrapper">
+        <p>The Mauritius Makers community is a group of enthusiasts and passionate people, who get their
+                        hands dirty by engineering things, enhancing existing things, finding solutions to problems
+                        etc.. Although makers encompasses a diverse range of disciplines or community is geared
+                        primarily toward technological innovation.</p>
+                    <p>If you are interested in SBC, SoC, Arduino, ESP8266, Raspberry Pi, GPIO, Intel Edison, sensors,
+                        actuators, IoT it would be a pleasure to have you in our community. Come join us at Developers
+                        Conference 2018.</p>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+
+</script>
+<style lang="scss" scoped>
+
+</style>
